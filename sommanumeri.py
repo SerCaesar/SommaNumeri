1ded45ff568b5b56eff2588ac762c30db6a2dabd
Ser Caesar <caesar@CaesarAirMac.local>
2025-03-07 16:03:02 +0100

    first commit

---
+++ sommanumeri.py
@@ -0,0 +1,12 @@
+def somma_due_numeri(a, b):
+    return a + b
+
+# Input dall'utente
+a = float(input("Inserisci il primo numero: "))
+b = float(input("Inserisci il secondo numero: "))
+
+# Calcolo della somma
+risultato = somma_due_numeri(a, b)
+
+# Output del risultato
+print(f"La somma di {a} e {b} è {risultato}")
